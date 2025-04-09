f0cee036ff47a6fbb40784512e5e7544812a7072
Loading <traftae@gmail.com>
2025-04-08 21:16:59 -0400

    Added FancyMenu files

--- src/main/java/com/loxding/fwendo/item/ModFoodComponents.java
+++ src/main/java/com/loxding/fwendo/item/ModFoodComponents.java
@@ -4,5 +4,5 @@ import net.minecraft.item.FoodComponent;
 
 public class ModFoodComponents {
     public static final FoodComponent CHEVON = new FoodComponent.Builder().hunger(2).saturationModifier(0.3f).build();
-    public static final FoodComponent COOKED_CHEVON = new FoodComponent.Builder().hunger(2).saturationModifier(0.3f).build();
+    public static final FoodComponent COOKED_CHEVON = new FoodComponent.Builder().hunger(6).saturationModifier(0.8f).build();
 }
